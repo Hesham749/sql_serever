fb95ba9a52b27f1731ab2a69fcc74b82be861bba
Hesham749 <heesham749@gmail.com>
2024-11-17 14:57:09 +0200

    Fix SQL query to correctly calculate percentage of vehicles with null door count

--- abo hadhoud/course 17/query21.sql
+++ abo hadhoud/course 17/query21.sql
@@ -4,6 +4,6 @@ USE VehicleMakesDB
 
 SELECT(
     (SELECT COUNT(*)
-    FROM VehicleDetails)*1.0 / (SELECT COUNT(*) AS [total vehicles]
     FROM VehicleDetails AS v
-    WHERE v.NumDoors is NULL )*1.0) AS PERCENTAGE
+    WHERE v.NumDoors is NULL )*1.0 /  (SELECT COUNT(*)
+    FROM VehicleDetails)*1.0 ) AS PERCENTAGE
