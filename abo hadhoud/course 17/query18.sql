4d3757926edf9051532da2bd9e8e2d600b84e23f
Hesham749 <heesham749@gmail.com>
2024-11-17 14:36:04 +0200

    Refactor SQL query to count FWD vehicle makes and add new query for total makes

--- abo hadhoud/course 17/query18.sql
+++ abo hadhoud/course 17/query18.sql
@@ -1,12 +1,10 @@
 USE VehicleMakesDB
 
 SELECT COUNT(*) AS [total makes]
-FROM
-(
-SELECT m.Make
-FROM VehicleDetails AS v JOIN Makes AS m
-    ON v.MakeID = m.MakeID
+FROM VehicleDetails AS v
     JOIN DriveTypes AS d
     ON v.DriveTypeID = d.DriveTypeID
 WHERE d.DriveTypeName = 'FWD'
-GROUP BY m.Make) r1
+
+
+
