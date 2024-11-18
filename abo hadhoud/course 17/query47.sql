497b5022318a044d52f164bffdc0fff1c76ea2a4
Hesham749 <heesham749@gmail.com>
2024-11-19 00:43:50 +0200

    Add query to retrieve the make with the highest number of vehicle models

--- abo hadhoud/course 17/query47.sql
+++ abo hadhoud/course 17/query47.sql
@@ -2,10 +2,19 @@ USE VehicleMakesDB
 
 
 
-
+--46
 SELECT TOP 3
     m.Make, COUNT(*) AS [number of models]
 FROM MakeModels  AS mm JOIN Makes AS m
     ON mm.MakeID = m.MakeID
 GROUP BY m.Make
 ORDER BY [number of models] DESC
+
+-- 47
+
+SELECT TOP 1
+    COUNT(*) AS [max number of models]
+FROM MakeModels  AS mm JOIN Makes AS m
+    ON mm.MakeID = m.MakeID
+GROUP BY m.Make
+ORDER BY [max number of models] DESC
