53faca4345cb6b9eb042ad668cd2f327c0e9ab29
Hesham749 <heesham749@gmail.com>
2024-11-19 01:13:54 +0200

    Refactor queries to retrieve the make with the fewest vehicle models and add new query for the same

--- abo hadhoud/course 17/query49.sql
+++ abo hadhoud/course 17/query49.sql
@@ -1,8 +1,10 @@
 USE VehicleMakesDB
 
+
+
 SELECT Make , [number of models]
 FROM (SELECT
-        Make, COUNT(*) AS [number of models] , DENSE_RANK() OVER (ORDER BY count(*) DESC) AS dr
+        Make, COUNT(*) AS [number of models] , DENSE_RANK() OVER (ORDER BY count(*) ASC) AS dr
     FROM MakeModels  AS mm JOIN Makes AS m
         ON mm.MakeID = m.MakeID
     GROUP BY m.Make)r1
