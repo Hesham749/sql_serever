dc2a5e6231a30f8578aaeb12d502590dea122a53
Hesham749 <heesham749@gmail.com>
2024-11-18 16:31:19 +0200

    functions

--- ITI/d7/fucnctions.sql
+++ ITI/d7/fucnctions.sql
@@ -12,7 +12,7 @@ SELECT @@ERROR
 
 --! save multiple value in variable
 
-DECLARE @t TABLE (x int)
+DECLARE @t TABLE (x INT)
 INSERT INTO @t
 SELECT Ins_Id
 FROM Instructor
@@ -33,9 +33,67 @@ FROM @t
 -- CASE
 -- IIF
 -- WAITFOR
--- CHOOSE   
+-- CHOOSE
 
-IF exists(select *
+IF EXISTS(SELECT name
 FROM sys.tables
-where name = 'Course')
+WHERE name = 'Course')
   SELECT 'table is exist'
+
+
+
+
+SELECT TOP(1)
+  i.Ins_Name
+FROM Instructor AS i
+ORDER BY LEN( i.Ins_Name) DESC
+
+
+
+-- scalar function
+CREATE FUNCTION getIName(@id INT)
+RETURNS VARCHAR(50)
+BEGIN
+  RETURN(SELECT i.Ins_Name
+  FROM Instructor AS i
+  WHERE i.Ins_Id = @id)
+END
+
+SELECT dbo.getIName(2)
+
+
+
+-- multiline function
+CREATE FUNCTION GetIns(@did INT)
+ RETURNS TABLE
+ RETURN(SELECT *
+FROM Instructor
+WHERE Dept_Id = @did)
+
+SELECT *
+FROM GetIns(20)
+
+
+-- multi statement function
+
+CREATE  FUNCTION getStd(@format VARCHAR(20))
+RETURNS @t TABLE
+(
+  id INT ,
+  ename VARCHAR(20)
+)
+BEGIN
+  IF @format ='first'
+    INSERT INTO @t
+  SELECT st_id , St_Fname
+  FROM Student
+
+  ELSE IF @format = 'last'
+     INSERT INTO @t
+  SELECT St_Id , St_Lname
+  FROM Student
+  RETURN
+END
+
+SELECT *
+FROM getStd('last')
