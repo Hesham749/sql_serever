88028799a628d95911928bb511ab715e85fb37b3
Hesham749 <heesham749@gmail.com>
2024-11-27 22:01:51 +0200

    Add cursor implementation to concatenate student first names

--- ITI/d10/in Lec.sql
+++ ITI/d10/in Lec.sql
@@ -26,4 +26,29 @@ CLOSE c1
 DEALLOCATE c1
 
 
- 
+-- second ex
+
+
+DECLARE c1 CURSOR
+ FOR
+ SELECT
+    St_Fname
+FROM
+    Student
+WHERE St_Fname IS
+NOT NULL
+FOR
+READ
+only
+DECLARE @name VARCHAR (20) , @allNames VARCHAR(max) =''
+OPEN c1
+FETCH c1 INTO @name
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @allNames = CONCAT(@allNames , ',',@name)
+    FETCH c1 INTO @name
+END
+SELECT
+    @allNames
+CLOSE c1
+DEALLOCATE c1
