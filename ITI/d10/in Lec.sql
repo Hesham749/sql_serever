e9c2d085bfdeb45aa1c5ee957f558a22a95d2800
Hesham749 <heesham749@gmail.com>
2024-11-27 21:47:06 +0200

    Add cursor implementation for fetching student records

--- ITI/d10/in Lec.sql
+++ ITI/d10/in Lec.sql
@@ -1,1 +1,29 @@
-use ITI
+USE ITI
+
+
+DECLARE c1 CURSOR
+FOR SELECT
+    St_Id ,
+    St_Fname
+FROM
+    Student
+FOR
+READ
+ONLY
+-- update is the default
+DECLARE @id INT , @name VARCHAR(20)
+OPEN c1
+FETCH c1
+INTO @id , @name
+WHILE @@FETCH_STATUS =0
+BEGIN
+    SELECT
+        @id ,
+        @name
+    FETCH c1 INTO @id , @name
+END
+CLOSE c1
+DEALLOCATE c1
+
+
+ 
